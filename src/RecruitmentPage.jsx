b22a551296f57069ff2e7f962a59b04d7edbf3f1
WRR-Enchantment <wrr.enchantment@gmail.com>
2025-07-12 22:46:31 +0700

    Update RecruitmentPage with animations and final layout

--- src/RecruitmentPage.jsx
+++ src/RecruitmentPage.jsx
@@ -1,7 +1,7 @@
 // RecruitmentPage.jsx
 {/* Background Layer */}
 <div
-  className="absolute top-0 left-0 w-full h-[200vh] -z-10 bg-top bg-no-repeat bg-cover"
+  className="fixed top-0 left-0 w-full min-h-[3000px] bg-cover bg-center bg-no-repeat z-[-1]"
   style={{ backgroundImage: "url('/bg.jpg')" }}
 ></div>
 
