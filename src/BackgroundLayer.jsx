7fe58f132ce9e4f9bdc4a80823dbcf2fce00f613
WRR-Enchantment <wrr.enchantment@gmail.com>
2025-07-13 16:04:54 +0700

    Update RecruitmentPage with animations and final layout

--- src/BackgroundLayer.jsx
+++ src/BackgroundLayer.jsx
@@ -1,17 +1,15 @@
 // BackgroundLayer.jsx
 export default function BackgroundLayerShape() {
   return (
-    <div
-      className="fixed bottom-0 left-0 w-full h-full z-0"
-     style={{
-     backgroundImage: "url('/bg.jpg')",
-     backgroundSize: "contain",
-     backgroundRepeat: "no-repeat",
-     backgroundPosition: "bottom center",
-     pointerEvents: "none",
-     transform: "scale(4.3)", // 🔍 perbesar 120%
-     transformOrigin: "bottom center", // penting: agar scaling tetap dari bawah
-}}
+  <img
+  src="/bg.jpg"
+  alt="Background"
+  className="fixed bottom-0 left-1/2 transform -translate-x-1/2 z-0 select-none pointer-events-none"
+  style={{
+    width: '100vw',
+    height: 'auto',
+    objectFit: 'contain',
+  }}
     />
   );
 }
