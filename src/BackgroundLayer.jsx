ef68d8c94b26d619ad53ca9b677ea8e5f62ad10f
WRR-Enchantment <wrr.enchantment@gmail.com>
2025-07-13 16:15:43 +0700

    Update RecruitmentPage with animations and final layout

--- src/BackgroundLayer.jsx
+++ src/BackgroundLayer.jsx
@@ -1,10 +1,10 @@
 // BackgroundLayer.jsx
 export default function BackgroundLayerShape() {
   return (
-   <img
-      src="/bg.jpg"
-      alt="Background"
-      className="fixed top-0 left-0 w-screen h-screen object-cover z-[-10] select-none pointer-events-none"
+  <img
+  src="/bg.jpg"
+  alt="Background"
+  className="fixed inset-0 w-full h-full object-cover z-[-10] select-none pointer-events-none"
   style={{
     width: '100vw',
     height: 'auto',
